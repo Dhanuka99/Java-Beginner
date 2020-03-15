c968e37b2a661ce4e9cb0a1af46486af414734d0
Dhanuka99 <danuka1999lakshan@gmail.com>
2020-03-15 16:42:42 +0530

    Addition Program

---
+++ src/day/two/Demo.java
@@ -0,0 +1,22 @@
+package day.two;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input number 1 : ");
+        int num1 = input.nextInt();
+        System.out.print("Input number 2 : ");
+        int num2 = input.nextInt();
+
+        int result;
+        result = num1 + num2;
+
+        System.out.println(result);
+    }
+}
+
+
+
+
